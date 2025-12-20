5bcea5fb49bade8cd4e2062ecfc9220d9d6984b0
mayukh-7 <mayukhmaity7@gmail.com>
2025-12-20 17:25:44 +0530

    Fix import case sensitivity for Auth middleware

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/Auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import {
   createPost,
